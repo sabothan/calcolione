ccdfdde614b1102df155f74c9bcaa96498a5a453
Jonathan Seeber <seeber.joni@outlook.de>
2026-05-19 02:14:22 +0200

    Ruff formatting

--- calcoglione/main.py
+++ calcoglione/main.py
@@ -7,7 +7,7 @@ from typing import Optional
 ALLOWED_SYMBOLS = re.compile(r"^[.,/*a-zA-Z0-9 \-+%°^()]+$")
 
 
-class UI():
+class UI:
     def __init__(self):
         pass
 
@@ -15,25 +15,25 @@ class UI():
         pass
 
 
-class QVar():
-    """Represents an arbitrary variable. Converts the variable into SI units for further calculations
-    """
+class QVar:
+    """Represents an arbitrary variable. Converts the variable into SI units for further calculations"""
+
     def __init__(
-            self,
-            name:str = "",
-            description:str = "",
-            type:str = "",
-            value:Optional[float] = None,
-            unit:str = "",
-        ):
+        self,
+        name: str = "",
+        description: str = "",
+        type: str = "",
+        value: Optional[float] = None,
+        unit: str = "",
+    ):
         self.name = name
         self.description = description
         self.type = type
         self.value = value
         self.unit = unit
-    
+
     @classmethod
-    def from_dict(cls, variable:dict):
+    def from_dict(cls, variable: dict):
         """Serves as an additional constructor, but with input from a `dict`.
 
         Args:
@@ -43,24 +43,23 @@ class QVar():
             The instantiated class.
         """
         return cls(
-            name = variable["name"],
-            description = variable["description"],
-            type = variable["type"],
-            value = variable["value"],
-            unit = variable["unit"],
+            name=variable["name"],
+            description=variable["description"],
+            type=variable["type"],
+            value=variable["value"],
+            unit=variable["unit"],
         )
 
     def _convert_to_si_units(self):
-        """This function converts an arbitrary value with arbitrary units into SI units.
-        """
+        """This function converts an arbitrary value with arbitrary units into SI units."""
         #  TODO: implement si converting
         pass
 
 
-class Question():
-    """Wrapper class to hold a question and it's information.
-    """
-    def __init__(self, message:str, vars:list[QVar]):
+class Question:
+    """Wrapper class to hold a question and it's information."""
+
+    def __init__(self, message: str, vars: list[QVar]):
         self.message = message
         self.vars = vars
 
@@ -68,7 +67,7 @@ class Question():
         self.message = self._render_message()
 
     @classmethod
-    def from_dict(cls, question:dict):
+    def from_dict(cls, question: dict):
         """Serves as an additional constructor, but with input from a `dict`.
 
         Args:
@@ -78,13 +77,13 @@ class Question():
             The instantiated class.
         """
         return cls(
-            message = question["message"],
-            vars = [QVar.from_dict(var) for var in question["vars"]],
+            message=question["message"],
+            vars=[QVar.from_dict(var) for var in question["vars"]],
         )
 
     def _render_message(self):
-        substitutions =  {}
-        
+        substitutions = {}
+
         # Generate subsitutions prompts for the message string
         for var in self.vars:
             substitutions[f"{var.name}.value"] = var.value
@@ -95,7 +94,7 @@ class Question():
         # Substitute <value, unit> tuples into messages
         for key, val in substitutions.items():
             message = message.replace("{" + key + "}", str(val))
-        
+
         return message
 
     def _get_question(self) -> str:
@@ -105,10 +104,10 @@ class Question():
             str: the question message
         """
         return self.message
-        
 
-class Answer():
-    def __init__(self, answer_type:str, calculation:str, result:QVar):
+
+class Answer:
+    def __init__(self, answer_type: str, calculation: str, result: QVar):
         self._answer_type = answer_type
         self._calculation = calculation
         self.result = result
@@ -118,9 +117,9 @@ class Answer():
 
         # Post-init processing
         # TODO: implement parsing
-    
+
     @classmethod
-    def from_dict(cls, answer:dict):
+    def from_dict(cls, answer: dict):
         """Serves as an additional constructor, but with input from a `dict`.
 
         Args:
@@ -130,9 +129,9 @@ class Answer():
             The instantiated class.
         """
         return cls(
-            answer_type = answer["answer_type"],
-            calculation = answer["calculation"],
-            result = QVar.from_dict(answer["result"]),
+            answer_type=answer["answer_type"],
+            calculation=answer["calculation"],
+            result=QVar.from_dict(answer["result"]),
         )
 
     def _input_answer(self) -> QVar:
@@ -144,14 +143,14 @@ class Answer():
             QVar: a variable object containing the provided answer
         """
         # Prompt an answer
-        my_answer:str = input("Answer: ")
+        my_answer: str = input("Answer: ")
 
         # Parse the answer into numeric and unit and pack into QVar
         parsed_answer = self._parse(my_answer)
 
         return parsed_answer
 
-    def _parse(self, answer:str) -> QVar:
+    def _parse(self, answer: str) -> QVar:
         """Parses an answer string into a QVar object.
         The string is split into a tuple <numeric, unit> accordingly and
         packed into the QVar object.
@@ -167,12 +166,12 @@ class Answer():
         """
         # Numeric type: e.g. 1425.345 km/h
         if self._answer_type == "numeric":
-            parsed_answer = self._parse_numeric(answer)   # value + unit
+            parsed_answer = self._parse_numeric(answer)  # value + unit
         # Expression type: e.g. 2*sin(34) * (2pi/rad)
         elif self._answer_type == "expression":
             parsed_answer = self._parse_expression(answer)  # sympy or similar
             raise NotImplementedError("This functionality is not implemented yet")
-        
+
         return parsed_answer
 
     def _parse_numeric(self, answer) -> QVar:
@@ -205,7 +204,7 @@ class Answer():
 
         # Split up answer into numeric and unit
         value_str = match.group(1).replace(",", ".")
-        unit_str  = match.group(2).strip()
+        unit_str = match.group(2).strip()
 
         # Convert the string numeric into a float
         try:
@@ -224,13 +223,14 @@ class Answer():
     def _parse_expression(self, answer) -> QVar:
         # TODO: implement scientific expressions
         return QVar()
-    
-    
-class Exercise():
+
+
+class Exercise:
     """Wrapper class for a Question and an Answer.
     Implements downstream methods for user interaction.
     """
-    def __init__(self, question:Question, answer:Answer):
+
+    def __init__(self, question: Question, answer: Answer):
         self.question = question
         self.answer = answer
 
@@ -251,24 +251,23 @@ class Exercise():
 
         # TODO: implement randomising values
         return cls(
-            question = Question.from_dict(exercise["question"]),
-            answer = Answer.from_dict(exercise["answer"])
+            question=Question.from_dict(exercise["question"]),
+            answer=Answer.from_dict(exercise["answer"]),
         )
- 
+
     def display_question(self):
-        """Print the question body to the terminal
-        """
+        """Print the question body to the terminal"""
         print(self.question._get_question())
-    
+
     def input_answer(self):
-        """Prompt the user for an answer.
-        """
+        """Prompt the user for an answer."""
         self.user_answer._input_answer()
-    
+
     def evaluate_answer(self):
-        """Evaluate the given answer for correctness.
-        """
-        print(f"Given answer: {self.user_answer.result.value} {self.user_answer.result.unit}")
+        """Evaluate the given answer for correctness."""
+        print(
+            f"Given answer: {self.user_answer.result.value} {self.user_answer.result.unit}"
+        )
         print(f"Correct answer: {self.answer.result.value} {self.answer.result.unit}")
 
         # TODO: finish evaluation
