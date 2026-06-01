b53f958062ea10690cf83e362b90af98f4f19e5f
Jonathan Seeber <seeber.joni@outlook.de>
2026-06-01 09:31:29 +0200

    Fix circular imports

--- calcolione/__init__.py
+++ calcolione/__init__.py
@@ -1,6 +1,2 @@
-from .answer import Answer
 from .exercise import Exercise
-from .question import Question
-from .qvar import QVar
 from .ui import UI
-from .utils import ALLOWED_SYMBOLS, NUMERIC_UNIT_PATTERN
