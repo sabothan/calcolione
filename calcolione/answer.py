18a12057880d412f5de738ac1386ed911af0ffd9
Jonathan Seeber <seeber.joni@outlook.de>
2026-06-18 10:32:33 +0200

    Add abs() answer evaluation

--- calcolione/answer.py
+++ calcolione/answer.py
@@ -163,4 +163,4 @@ class Answer:
         correct_answer = self.result.quantity.to_base_units().magnitude
 
         # TODO implement more stable approach to evaluate the answer's correctness
-        return ((given_answer - correct_answer) <= ANSWER_TOLERANCE)
+        return abs(given_answer - correct_answer) <= ANSWER_TOLERANCE
